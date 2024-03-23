2b4497588dc4b3529054c6b4fad93b1ef4368483
Walter Eschenbach <dabears1.1.1@gmail.com>
2024-03-23 13:21:27 -0400

    created top nav bar

--- src/screens/MyInfoScreen.js
+++ src/screens/MyInfoScreen.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import PersonalScreen from './PersonalScreen';
+import JobScreen from './JobScreen';
+import TopNavBar from '../navigation/TopNavBar';
+import TimeOffScreen from './TimeOffScreen';
+import DocumentsScreen from './DocumentsScreen';
+import BenefitsScreen from './BenefitsScreen';
+import PerformanceScreen from './PerformanceScreen';
+import NotesScreen from './NotesScreen';
+
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function MyInfoScreen() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>My Info Screen</Text>
-        </View>
+        <Tab.Navigator
+            tabBar={(props) => <TopNavBar {...props} />}>
+            <Tab.Screen name="Personal" component={PersonalScreen} />
+            <Tab.Screen name="Job" component={JobScreen} />
+            <Tab.Screen name="Time Off" component={TimeOffScreen} />
+            <Tab.Screen name="Documents" component={DocumentsScreen} />
+            <Tab.Screen name="Benefits" component={BenefitsScreen} />
+            <Tab.Screen name="Performance" component={PerformanceScreen} />
+            <Tab.Screen name="Notes" component={NotesScreen} />
+        </Tab.Navigator>
     );
 }
 
