806a5098564b8b5eadd04083aa6504b69af118d9
Walter Eschenbach <dabears1.1.1@gmail.com>
2024-03-23 19:11:44 -0400

    initial draft of indbox screen

--- src/screens/InboxScreen.js
+++ src/screens/InboxScreen.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    SectionList,
+    StatusBar
+} from 'react-native';
+import inboxData from '../assets/json/inbox.json';
+import { Icon } from 'react-native-elements';
+
 
 export default function InboxScreen() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Inbox Screen</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <SectionList
+                style={styles.listContainer}
+                sections={inboxData}
+                keyExtractor={(item, index) => item + index}
+                renderItem={({ item }) => (
+                    <View style={styles.item}>
+                        <Icon name='user' type='font-awesome' style={styles.image} />
+                        <View style={styles.textContainer}>
+
+                            <Text style={styles.title}>{item.request}</Text>
+                            <Text style={styles.name}>{item.name}</Text>
+                            <Text style={styles.date}>{item.date}</Text>
+                        </View>
+                    </View>
+                )}
+                renderSectionHeader={({ section: { title } }) => (
+                    <Text style={styles.header}>{title}</Text>
+                )}
+            />
+        </SafeAreaView>
     );
 }
 
@@ -15,10 +43,40 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+        paddingTop: StatusBar.currentHeight,
+        width: '100%',
     },
-    text: {
-        fontSize: 20,
-        textAlign: 'center',
+    listContainer: {
+        width: '100%',
+    },
+    item: {
+        flexDirection: 'row',
+        padding: 10,
+        bottomBorderWidth: 1,
+        borderBottomColor: '#ccc',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+    },
+    image: {
+        width: 50,
+        height: 50,
+        borderWidth: 1,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textContainer: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        flex: 1,
         margin: 10,
+    },
+    header: {
+        fontSize: 18,
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 14,
     },
 });
